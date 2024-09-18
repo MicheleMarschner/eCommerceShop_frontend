90c46212bb702a6f0946d693297b2ff8e66f46a3
Michele Marschner <michele.marschner@gmail.com>
2024-09-18 12:33:16 +0200

    commit for vercel deployment

---
+++ app/api/webhook/route.js
@@ -0,0 +1,70 @@
+import { NextResponse } from 'next/server';
+import { mongooseConnect } from '@/app/lib/mongoose';
+
+const stripe = require('stripe')(process.env.STRIPE_SK);
+import { Order } from '@/app/models/Order';
+
+export async function POST(req, res) {
+
+    await mongooseConnect();
+    let sig; 
+
+    for (let [key, value] of req.headers.entries()) { 
+        if (key.toLowerCase() === 'stripe-signature') {
+            sig = value;
+        }
+    } 
+
+    let event;
+    const body = await req.text()
+    const endpointSecret = process.env.STRIPE_ENDPOINT_SK;
+
+    if (endpointSecret) {
+        
+        try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            sig,
+            endpointSecret
+        );
+        } catch (err) {
+            console.log('⚠️  Webhook signature verification failed.', err.message);
+            return NextResponse.json(400);
+        }
+    }
+
+    let data = {};
+    let orderId = '';
+    let paid = false;
+
+    // Handle the event
+    switch (event.type) {
+        case 'checkout.session.completed':
+            //data = event.data.object;
+            orderId = object.metadata.orderId;
+            paid = object.payment_status === 'paid';
+
+            if (orderId && paid) {
+                await Order.findByIdAndUpdate(orderId, {paid: true})
+            }
+
+            break;
+
+        case 'charge.updated':
+            data = event.data.object;
+            orderId = data.metadata.orderId;
+            paid = data.payment_status === 'paid';
+
+            if (orderId && paid) {
+                await Order.findByIdAndUpdate(orderId, {paid: true})
+            }
+
+            break;
+          
+        default:
+        // Unexpected event type
+            console.log(`Unhandled event type ${event.type}.`);
+    }
+    
+    return NextResponse.json(200);
+}
